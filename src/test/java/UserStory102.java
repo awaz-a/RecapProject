8663d126222b81fd1a06c46e19856b511a1ed7f0
Awaz <82914552+awaz-a@users.noreply.github.com>
2021-07-11 20:13:20 -0500

    Us102 has been completed (#2)
    combined all 3 commits by squashing

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,7 @@
+public class UserStory102 {
+    //lots of code
+    // ready to push this branch to remote
+    // and open pull request so i can start collaborating
+    //so my team member can comment, review and approve or request for more changes
+    //more changes
+}
